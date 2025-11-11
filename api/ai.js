07df1102cfce6b7605fe1f876cf2de4ae2d4e048
quanghuy2701git <trinhquanghuy2701@gmail.com>
2025-11-12 00:04:35 +0700

    fixed CORS

--- api/ai.js
+++ api/ai.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  
+	if (req.method == "OPTIONS") {
+	  return res.status(200).end();
+	}
+
 	const {prompt} = req.query;
 	const apiKey = process.env.GOOGLE_API_KEY;
 	const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
