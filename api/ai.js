c7f0822565b0ce79fbd44566bba87a874c7776b8
quanghuy2701git <trinhquanghuy2701@gmail.com>
2025-12-08 21:05:12 +0700

    forgot to add

--- api/ai.js
+++ api/ai.js
@@ -14,19 +14,21 @@ export default async function handler(req, res) {
   	}
 
 	const apiKey = process.env.GOOGLE_API_KEY;
-	const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
+	const url = `https://api.poe.com/v1/chat/completions`;
 	
 	const requestBody = {
-		contents: [{
-			role: "user",
-			parts: [{ text: decodeURIComponent(prompt) }],
-		}]
+		model: "writingmastergen",
+    messages: [{
+      role: "user",
+      content: decodeURIComponent(prompt)
+    }]
 	};
 	
 	const response = await fetch(url, {
 		method: "POST",
 		headers: {
-		"Content-Type": "application/json",
+		  "Content-Type": "application/json",
+      "Authorization": `Bearer ${apiKey}`,
 		},
 		body: JSON.stringify(requestBody),
 	});
